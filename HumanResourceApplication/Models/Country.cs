0b7745db83c9c796613ea8b431986fc6a4834a28
Rishav Sharma <rishav.sharma@capgemini.com>
2024-12-18 11:23:59 +0530

    Models Updated

---
+++ HumanResourceApplication/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResourceApplication.Models;
+
+public partial class Country
+{
+    public string CountryId { get; set; } = null!;
+
+    public string? CountryName { get; set; }
+
+    public decimal? RegionId { get; set; }
+
+    public virtual ICollection<Location> Locations { get; set; } = new List<Location>();
+
+    public virtual Region? Region { get; set; }
+}
