114633ccd205550f2124931cecbe6e945af36152
Gandru Akhila <gandru.akhila@capgemini.com>
2024-12-20 14:32:02 +0530

    summary,region added

--- HumanResourceApplication/Controllers/CountryController.cs
+++ HumanResourceApplication/Controllers/CountryController.cs
@@ -20,9 +20,15 @@ namespace HumanResourceApplication.Controllers
             _countryRepository = countryRepository;
             _countryValidator = validator;
         }
-       
 
-        
+
+        #region Get All Countries
+
+        /// <summary>
+        /// Retrieves a list of all countries from the database.
+        /// </summary>
+        /// <returns>A list of CountryDTO objects or a NotFound response if no countries are found.</returns>
+
         [HttpGet]
         public async Task<ActionResult<List<CountryDTO>>> GetAllCountries()
         {
@@ -41,9 +47,16 @@ namespace HumanResourceApplication.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
 
 
+        #region Get Country By Id
 
+        /// <summary>
+        /// Retrieves a specific country by its ID from the database.
+        /// </summary>
+        /// <param name="Countryid">The ID of the country to retrieve.</param>
+        /// <returns>A CountryDTO object if found, otherwise a NotFound response.</returns>
         [HttpGet("id")]
         public async Task<ActionResult<CountryDTO>> GetCountryById(string Countryid)
         {
@@ -62,6 +75,15 @@ namespace HumanResourceApplication.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
+
+        #region Add Country
+
+        /// <summary>
+        /// Adds a new country to the database.
+        /// </summary>
+        /// <param name="country">The CountryDTO object containing the new country data.</param>
+        /// <returns>A success message or a BadRequest response if validation fails.</returns>
 
         [HttpPost]
         public async Task<IActionResult> AddCountry(CountryDTO country)
@@ -71,10 +93,7 @@ namespace HumanResourceApplication.Controllers
                 var validationResult =await _countryValidator.ValidateAsync(country);
                 if (!validationResult.IsValid)
                 {
-
                     return BadRequest(validationResult.Errors);
-
-
                 }
                 await _countryRepository.AddCountry( country);
                 return Ok("Record added successfully");
@@ -84,6 +103,16 @@ namespace HumanResourceApplication.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
+
+        #region Update Country
+
+        /// <summary>
+        /// Updates an existing country in the database.
+        /// </summary>
+        /// <param name="Countryid">The ID of the country to update.</param>
+        /// <param name="country">The CountryDTO object containing the updated country data.</param>
+        /// <returns>A success message or a BadRequest response if validation fails.</returns>
         [HttpPut]
         public async Task<IActionResult> UpdateCountry(string Countryid ,CountryDTO country)
         {
@@ -104,7 +133,16 @@ namespace HumanResourceApplication.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
 
+        #region  Delete Country By Id
+
+        /// <summary>
+        /// Deletes a country by its ID from the database.
+        /// </summary>
+        /// <param name="id">The ID of the country to delete.</param>
+        /// <returns>A NoContent response on success, or a BadRequest response on failure.</returns>
+       
         [HttpDelete("id")]
         public async Task<IActionResult> DeleteCountryById(string id)
         {
@@ -118,8 +156,9 @@ namespace HumanResourceApplication.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        #endregion
 
 
     }
-    
+
 }
