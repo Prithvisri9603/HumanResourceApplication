7be41a9a86ce3f0de476d4ba8eeb6a5b551858ae
Beauty Kumari S <beauty.kumari-s@capgemini.com>
2024-12-23 10:16:36 +0530

    Add Region,Department

---
+++ HumanResourceApplication/Validators/DepartmentDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HumanResourceApplication.DTO;
+
+namespace HumanResourceApplication.Validators
+{
+    public class DepartmentDTOValidator : AbstractValidator<DepartmentDTO>
+    {
+        public DepartmentDTOValidator()
+        {
+            RuleFor(x => x.DepartmentId).NotEmpty().WithMessage("Department Id is required");
+            RuleFor(x => x.ManagerId).NotNull();
+            RuleFor(x => x.LocationId).NotNull();
+            
+        }
+    }
+}
